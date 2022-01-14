15bc4a715d7ed44ed0efa5681c6db432e711964b
Israel <Israe7manuel@gmail.com>
2022-01-14 14:03:07 +0300

    Initialize AddTask feature and job

---
+++ tests/Feature/AddTaskFeatureTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Features\AddTaskFeature;
+
+class AddTaskFeatureTest extends TestCase
+{
+    public function test_add_task_feature()
+    {
+        $this->markTestIncomplete();
+    }
+}
---
+++ tests/Unit/Domains/Task/Jobs/AddTaskJobTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Unit\Domains\Task\Jobs;
+
+use Tests\TestCase;
+use App\Domains\Task\Jobs\AddTaskJob;
+
+class AddTaskJobTest extends TestCase
+{
+    public function test_add_task_job()
+    {
+        $this->markTestIncomplete();
+    }
+}
